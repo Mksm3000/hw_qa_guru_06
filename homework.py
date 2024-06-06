e716f92a3a81b9b628e38b6b0520bbb6f5f3b631
Maksim Zosimov <mizo-y@yandex.ru>
2024-06-06 14:15:43 +0200

    исправлено - в test_dark_theme_by_time_and_user_choice удалено неверное условие

--- homework.py
+++ homework.py
@@ -25,15 +25,13 @@ def test_dark_theme_by_time_and_user_choice():
     dark_theme_enabled_by_user = None - Пользователь не сделал выбор (используется
     переключение по времени системы)
     """
-    current_time = time(hour=16)
-    dark_theme_enabled_by_user = True
+    current_time = time(hour=23)
+    dark_theme_enabled_by_user = False
     is_dark_theme = None
 
     if dark_theme_enabled_by_user:
         is_dark_theme = True
-    elif not dark_theme_enabled_by_user:
-        is_dark_theme = False
-    elif dark_theme_enabled_by_user is None:
+    else:
         if time(hour=6) <= current_time < time(hour=22):
             is_dark_theme = False
         else:
